1a91272a1c187e000736a501d2bdaa957ee31f32
rapter7717 <35534590+rapter7717@users.noreply.github.com>
2019-01-23 11:50:00 -0600

    Innital Commit
    Something is technically working here

---
+++ maincode.js
@@ -0,0 +1,10 @@
+alert("whoop!")
+var userName = prompt("Hi! What's your name?", "Albert");
+console.log(userName);
+var numCats = prompt("How many cats do you have?");
+console.log(numCats);
+var numCatsNum= Number(numCats);
+var num = numCatsNum*2;
+console.log(num);
+var students=["Adrian","Austin","Dakota"];
+console.log(students, 2);
