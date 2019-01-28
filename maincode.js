82f216c8505cca363b100fce3e5e1f1b8c02a773
rapter7717 <35534590+rapter7717@users.noreply.github.com>
2019-01-27 23:30:53 -0600

    Completed First Assignment

--- maincode.js
+++ maincode.js
@@ -1,10 +1,9 @@
-alert("whoop!")
 var userName = prompt("Hi! What's your name?", "Albert");
 console.log(userName);
-var numCats = prompt("How many cats do you have?");
+var numCats = prompt("How many cats do you have?", 800);
 console.log(numCats);
 var numCatsNum= Number(numCats);
 var num = numCatsNum*2;
 console.log(num);
 var students=["Adrian","Austin","Dakota"];
-console.log(students, 2);
+console.log(students);
