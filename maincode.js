fabea3735a676f1891b9935dd70b607b682dce66
rapter7717 <35534590+rapter7717@users.noreply.github.com>
2019-02-10 18:36:53 -0600

    Rubric 2 Complete

--- maincode.js
+++ maincode.js
@@ -13,15 +13,36 @@ var students=["Adrian","Austin","Dakota"];
 var instructors=["Susan"];
 console.log(students);
 function testFunction (userName){
-    var greeting= "Hello " + userName + "!";
+    var greeting= "Were so glad you're here " + userName + "!";
     return greeting;
 }
-document.write(testFunction(userName));
 if (userName == students[1] || userName == students[2] || userName == students[0]){
-    document.write (" I remember you!");
+    document.getElementsByClassName("javascriptOutput")[0].innerHTML=testFunction(userName) + " I remember you! Your crazy cat number is " + crazyFactor ;
 } else if (userName == instructors [0]) {
-    document.write (" I see you've come to check up on things...");
+    document.getElementsByClassName("javascriptOutput")[0].innerHTML=testFunction(userName) + " I see you've come to check up on things... Your crazy cat number is " + crazyFactor ;
 } else {
-    document.write (" Welcome!");
+    document.getElementsByClassName("javascriptOutput")[0].innerHTML=testFunction(userName) + " Welcome! Your crazy cat number is " + crazyFactor ;
 }
-document.write (" Your crazy cat number is " + crazyFactor)
+document.getElementsByTagName("H2")[0].innerHTML="Hello " + userName + "!";
+switch (true) {
+    case (crazyFactor > 400):
+        console.log ('These people are lying');
+        break;
+    case (crazyFactor < 400):
+        console.log ('These people might be telling the truth');
+        break;
+    case (crazyFactor = 400):
+        console.log ("That is improbable");
+        break;
+    default:
+        console.log ("Something didn't get done");
+}
+if (crazyFactor > 400) {
+    console.log ("Less than 400");
+} else if (crazyFactor < 400) {
+    console.log ("More than 400");
+} else if (crazyFactor = 400) {
+    console.log ("This didn't happen before");
+} else {
+    console.log ("The If statement didn't work");
+}
