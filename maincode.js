125d77e15a44a9281d806f219e0ae440cd5cc2e9
rapter7717 <35534590+rapter7717@users.noreply.github.com>
2019-02-24 14:51:53 -0600

    Clock Completed

--- maincode.js
+++ maincode.js
@@ -72,3 +72,29 @@ if ( userName == fullName[1] ||userName == fullName[2] ||userName == fullName[3]
     subTitle.appendChild(nothingSpecial);
 }
 
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+let hrPosition = (hr*360/12)+((min*(360/60)/12)+(sec*(360/60)/60));
+let minPosition = (min*360/60)+(sec*(360/60)/60);
+let secPosition = sec*360/60;
+
+function runClock () {
+
+    hrPosition = hrPosition+(3/360);
+    minPosition = minPosition+(6/60);
+    secPosition = secPosition+6;
+
+    const HOURHAND = document.querySelector("#hour");
+    const MINUTEHAND = document.querySelector("#minute");
+    const SECONDHAND = document.querySelector("#second");
+
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(runClock, 1000);
